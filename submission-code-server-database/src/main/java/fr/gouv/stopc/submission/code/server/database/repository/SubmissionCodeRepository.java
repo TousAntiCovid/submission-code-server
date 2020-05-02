4eda3739e2233c0fcc537cc533575974e284c331
Cacaoyer Tac <99999-Cacaoyer.tac@users.noreply.gitlab.inria.fr>
2020-05-02 14:46:16 +0200

    Implement get last lot value in database

--- submission-code-server-database/src/main/java/fr/gouv/stopc/submission/code/server/database/repository/SubmissionCodeRepository.java
+++ submission-code-server-database/src/main/java/fr/gouv/stopc/submission/code/server/database/repository/SubmissionCodeRepository.java
@@ -1,6 +1,7 @@
 package fr.gouv.stopc.submission.code.server.database.repository;
 
 import fr.gouv.stopc.submission.code.server.database.entity.SubmissionCode;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -9,5 +10,8 @@ public interface SubmissionCodeRepository extends CrudRepository<SubmissionCode,
      SubmissionCode findByCodeAndType(String code, String type);
 
      List<SubmissionCode> findAllByLotNullAndTypeEquals(String type);
+
+     @Query(value = "SELECT max(lot) FROM SubmissionCode ")
+     long lastLot();
 
 }
