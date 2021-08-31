a077299380322748906f461e4621a743e218d0ed
Jamalac Orange <14492-x-JaOrang@users.noreply.gitlab.inria.fr>
2021-08-31 14:20:47 +0000

    test(sftp): test sftp file presence

---
+++ src/test/java/fr/gouv/stopc/submission/code/server/sftp/IntegrationTest.java
@@ -0,0 +1,26 @@
+package fr.gouv.stopc.submission.code.server.sftp;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.TestPropertySource;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.test.context.TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS;
+
+@ActiveProfiles("dev")
+@TestPropertySource("classpath:application-dev.properties")
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@TestExecutionListeners(listeners = { SftpManager.class }, mergeMode = MERGE_WITH_DEFAULTS)
+@Retention(RUNTIME)
+@Target(TYPE)
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+public @interface IntegrationTest {
+}
