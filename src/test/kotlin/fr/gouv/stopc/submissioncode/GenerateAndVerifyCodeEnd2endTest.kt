40e2c1cf277ef4fcedc1fe9915a15a855e2f0df1
Jujube Orange <13631-x-JOrang@users.noreply.gitlab.inria.fr>
2022-05-04 07:58:04 +0000

    test: add edge case test

--- src/test/kotlin/fr/gouv/stopc/submissioncode/GenerateAndVerifyCodeEnd2endTest.kt
+++ src/test/kotlin/fr/gouv/stopc/submissioncode/GenerateAndVerifyCodeEnd2endTest.kt
@@ -3,10 +3,10 @@ package fr.gouv.stopc.submissioncode
 import fr.gouv.stopc.submissioncode.test.IntegrationTest
 import fr.gouv.stopc.submissioncode.test.When
 import io.restassured.RestAssured.given
-import org.hamcrest.Matchers
+import org.hamcrest.Matchers.equalTo
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
-import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatus.OK
 
 @IntegrationTest
 class GenerateAndVerifyCodeEnd2endTest {
@@ -19,7 +19,7 @@ class GenerateAndVerifyCodeEnd2endTest {
         ]
     )
     fun a_code_generated_with_the_api_can_be_verified(generatePath: String) {
-        val shortCode = given()
+        val code = given()
             .get(generatePath)
             .then()
             .extract()
@@ -27,10 +27,10 @@ class GenerateAndVerifyCodeEnd2endTest {
             .getString("code")
 
         When()
-            .get("/api/v1/verify?code={code}", shortCode)
+            .get("/api/v1/verify?code={code}", code)
 
             .then()
-            .statusCode(HttpStatus.OK.value())
-            .body("valid", Matchers.equalTo(true))
+            .statusCode(OK.value())
+            .body("valid", equalTo(true))
     }
 }
