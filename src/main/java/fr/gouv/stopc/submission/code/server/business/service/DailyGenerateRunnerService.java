bdcd331ce4537102e893c0e914063cd80439a3e0
Sapotille Orange <15519-x-SaOrang@users.noreply.gitlab.inria.fr>
2022-05-05 10:56:12 +0200

    feature: prevent daily code generation runner to start in integration test

--- src/main/java/fr/gouv/stopc/submission/code/server/business/service/DailyGenerateRunnerService.java
+++ src/main/java/fr/gouv/stopc/submission/code/server/business/service/DailyGenerateRunnerService.java
@@ -3,10 +3,12 @@ package fr.gouv.stopc.submission.code.server.business.service;
 import fr.gouv.stopc.submission.code.server.business.service.schedule.DailyGenerateSchedule;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Profile("!dev")
 public class DailyGenerateRunnerService implements CommandLineRunner {
 
     private final DailyGenerateSchedule dailyGenerateSchedule;
