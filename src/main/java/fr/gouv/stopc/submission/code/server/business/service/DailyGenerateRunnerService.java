4fef989dad4ff5fc0d3c68968ed2cd14fb0c4473
Sapotille Orange <15519-x-SaOrang@users.noreply.gitlab.inria.fr>
2022-05-06 15:38:56 +0200

    feat: add condition on runner

--- src/main/java/fr/gouv/stopc/submission/code/server/business/service/DailyGenerateRunnerService.java
+++ src/main/java/fr/gouv/stopc/submission/code/server/business/service/DailyGenerateRunnerService.java
@@ -3,12 +3,12 @@ package fr.gouv.stopc.submission.code.server.business.service;
 import fr.gouv.stopc.submission.code.server.business.service.schedule.DailyGenerateSchedule;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Profile;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-@Profile("!dev")
+@ConditionalOnProperty(name = "spring.main.web-application-type", havingValue = "none")
 public class DailyGenerateRunnerService implements CommandLineRunner {
 
     private final DailyGenerateSchedule dailyGenerateSchedule;
