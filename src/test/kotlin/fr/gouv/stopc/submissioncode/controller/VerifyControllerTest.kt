536a4aa79609179c82c7c7560fd88860db963426
Sapotille Orange <15519-x-SaOrang@users.noreply.gitlab.inria.fr>
2022-05-31 16:52:19 +0200

    feat: merge all tests for invalid jwt cases in one using methodSource and arguments + put all tests on /verify in the same file

--- src/test/kotlin/fr/gouv/stopc/submissioncode/controller/VerifyControllerTest.kt
+++ src/test/kotlin/fr/gouv/stopc/submissioncode/controller/VerifyControllerTest.kt
@@ -1,15 +1,33 @@
 package fr.gouv.stopc.submissioncode.controller
 
+import com.nimbusds.jose.JOSEObjectType
+import com.nimbusds.jose.JWSAlgorithm
+import com.nimbusds.jose.JWSHeader
+import com.nimbusds.jose.jwk.Curve
+import com.nimbusds.jose.jwk.gen.ECKeyGenerator
+import com.nimbusds.jwt.JWTClaimsSet
 import fr.gouv.stopc.submissioncode.test.IntegrationTest
+import fr.gouv.stopc.submissioncode.test.JWTManager.Companion.givenJwt
+import fr.gouv.stopc.submissioncode.test.JWTManager.Companion.givenJwtBuilder
 import fr.gouv.stopc.submissioncode.test.PostgresqlManager.Companion.givenTableSubmissionCodeContainsCode
 import fr.gouv.stopc.submissioncode.test.When
 import io.restassured.RestAssured.given
 import org.hamcrest.Matchers.equalTo
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS
 import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import org.junit.jupiter.params.provider.ValueSource
 import org.springframework.http.HttpStatus.OK
 import java.time.Instant
+import java.time.temporal.ChronoUnit
+import java.util.Date
+import java.util.UUID
+import java.util.stream.Stream
 
 @IntegrationTest
 class VerifyControllerTest {
@@ -65,27 +83,15 @@ class VerifyControllerTest {
             "",
             "    ",
             "a",
-            "bbbbbbbbbbbbbbbb"
-        ]
-    )
-    fun codes_with_invalid_length_are_rejected(validCodeButWrongCase: String) {
-        When()
-            .get("/api/v1/verify?code={code}", validCodeButWrongCase)
-
-            .then()
-            .statusCode(OK.value())
-            .body("valid", equalTo(false))
-    }
-
-    @ParameterizedTest
-    @ValueSource(
-        strings = [
+            "bbbbbbbbbbbbbbbb",
             "00000000-1111-1111-1111-111111111111",
             "AAA000",
-            "BBBBBB000000"
+            "BBBBBB000000",
+            "aaaaaaaa.aaaaaaaa",
+            "aaaaaaaaaa.aaaaaaaaa.aaaaaaaaa"
         ]
     )
-    fun can_detect_a_short_code_doesnt_exist(unexistingCode: String) {
+    fun can_detect_a_short_code_doesnt_exist_and_is_not_a_valid_JWT(unexistingCode: String) {
         When()
             .get("/api/v1/verify?code={code}", unexistingCode)
 
@@ -134,5 +140,162 @@ class VerifyControllerTest {
             .then()
             .statusCode(OK.value())
             .body("valid", equalTo(false))
+    }
+
+    @TestInstance(PER_CLASS)
+    @Nested
+    inner class JwtTest {
+
+        private fun generateInvalidJwt(): Stream<Arguments> {
+            return Stream.of(
+                Arguments.of(
+                    "reject a JWT issued more than 7 days in the past",
+                    givenJwt(issuedAt = Instant.now().minus(7, ChronoUnit.DAYS))
+                ),
+                Arguments.of(
+                    "reject a JWT issued in the future",
+                    givenJwt(issuedAt = Instant.now().plus(1, ChronoUnit.MINUTES))
+                ),
+                Arguments.of(
+                    "reject a JWT with an iat field as an empty string instead of a Date",
+                    givenJwtBuilder(
+                        claimsBuilder = JWTClaimsSet.Builder()
+                            .claim("iat", "")
+                            .claim("jti", UUID.randomUUID())
+                    )
+                ),
+                Arguments.of(
+                    "reject a JWT with an iat field as a blank string instead of a Date",
+                    givenJwtBuilder(
+                        claimsBuilder = JWTClaimsSet.Builder()
+                            .claim("iat", " ")
+                            .claim("jti", UUID.randomUUID())
+                    )
+                ),
+                Arguments.of(
+                    "reject a JWT with an iat field as a string instead of a Date",
+                    givenJwtBuilder(
+                        claimsBuilder = JWTClaimsSet.Builder()
+                            .claim("iat", "123456")
+                            .claim("jti", UUID.randomUUID())
+                    )
+                ),
+                Arguments.of(
+                    "reject a JWT with missing iat field",
+                    givenJwtBuilder(
+                        claimsBuilder = JWTClaimsSet.Builder()
+                            .jwtID("TousAntiCovidJti")
+                    )
+                ),
+                Arguments.of(
+                    "reject a JWT with an empty jti field",
+                    givenJwt(jti = "")
+                ),
+                Arguments.of(
+                    "reject a JWT with a blank jti field",
+                    givenJwt(jti = " ")
+                ),
+                Arguments.of(
+                    "reject a JWT with missing jti field",
+                    givenJwtBuilder(
+                        claimsBuilder = JWTClaimsSet.Builder()
+                            .issueTime(Date.from(Instant.now()))
+                    )
+                ),
+                Arguments.of(
+                    "reject a JWT with jti field as a number instead of a string",
+                    givenJwtBuilder(
+                        claimsBuilder = JWTClaimsSet.Builder()
+                            .claim("jti", 1234)
+                            .issueTime(Date.from(Instant.now()))
+                    )
+                ),
+                Arguments.of(
+                    "reject a JWT with an empty kid field ",
+                    givenJwt(kid = "")
+                ),
+                Arguments.of(
+                    "reject a JWT with a blank kid field ",
+                    givenJwt(kid = " ")
+                ),
+                Arguments.of(
+                    "reject a JWT with missing kid field ",
+                    givenJwtBuilder(
+                        headerBuilder = JWSHeader.Builder(JWSAlgorithm.ES256)
+                            .type(JOSEObjectType.JWT)
+                    )
+                ),
+                Arguments.of(
+                    "reject a JWT with unknown kid value",
+                    givenJwt(kid = "test")
+                ),
+                Arguments.of(
+                    "reject a JWT with a kid value associate to a wrong key",
+                    givenJwt(kid = "AnotherKID")
+                ),
+            )
+        }
+
+        @Test
+        fun validate_a_valid_JWT() {
+
+            val validJwt = givenJwt()
+
+            When()
+                .get("/api/v1/verify?code={jwt}", validJwt)
+
+                .then()
+                .statusCode(OK.value())
+                .body("valid", equalTo(true))
+        }
+
+        @Test
+        fun reject_a_JWT_sign_with_unknown_private_key() {
+
+            val unknownKeys = ECKeyGenerator(Curve.P_256)
+                .keyID("unknownKey")
+                .generate()
+
+            val jwtWithUnknownSignature = givenJwt(privateKey = unknownKeys.toECPrivateKey())
+
+            When()
+                .get("/api/v1/verify?code={jwt}", jwtWithUnknownSignature)
+
+                .then()
+                .statusCode(OK.value())
+                .body("valid", equalTo(false))
+        }
+
+        @ParameterizedTest(name = "{0}")
+        @MethodSource("generateInvalidJwt")
+        fun reject_JWT_with_invalid_value_or_structure(title: String, serializedJwt: String) {
+
+            When()
+                .get("/api/v1/verify?code={jwt}", serializedJwt)
+
+                .then()
+                .statusCode(OK.value())
+                .body("valid", equalTo(false))
+        }
+
+        @Test
+        fun reject_a_JWT_with_jti_already_used() {
+
+            val validJwt = givenJwt()
+
+            given()
+                .get("/api/v1/verify?code={jwt}", validJwt)
+
+                .then()
+                .statusCode(OK.value())
+                .body("valid", equalTo(true))
+
+            When()
+                .get("/api/v1/verify?code={jwt}", validJwt)
+
+                .then()
+                .statusCode(OK.value())
+                .body("valid", equalTo(false))
+        }
     }
 }
--- src/test/kotlin/fr/gouv/stopc/submissioncode/test/JWTManager.kt
+++ src/test/kotlin/fr/gouv/stopc/submissioncode/test/JWTManager.kt
@@ -16,112 +16,79 @@ import java.security.interfaces.ECPrivateKey
 import java.time.Instant
 import java.util.Base64
 import java.util.Date
-import java.util.UUID
 
 @Slf4j
 class JWTManager : TestExecutionListener {
 
     companion object {
 
-        private val defaultEcKey: ECKey
+        private val tacEcKey: ECKey
 
         init {
 
             try {
 
-                defaultEcKey = ECKeyGenerator(Curve.P_256)
+                tacEcKey = ECKeyGenerator(Curve.P_256)
                     .keyID("TousAntiCovidKID")
                     .generate()
 
-                val publicECKey = Base64.getEncoder()
-                    .encodeToString(defaultEcKey.toECPublicKey().encoded)
+                val tacPublicECKey = Base64.getEncoder()
+                    .encodeToString(tacEcKey.toECPublicKey().encoded)
+
+                val anotherEcKey = ECKeyGenerator(Curve.P_256)
+                    .keyID("AnotherKID")
+                    .generate()
+
+                val anotherPublicEcKey = Base64.getEncoder()
+                    .encodeToString(anotherEcKey.toECPublicKey().encoded)
 
                 System.setProperty(
-                    "submission.code.server.publicKeys.TousAntiCovidKID",
-                    publicECKey
+                    "submission.publicKeys.TousAntiCovidKID",
+                    tacPublicECKey
                 )
                 System.setProperty(
-                    "submission.code.server.publicKeys.D99DA4422914F5E8",
-                    "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEosj0ewm2rrYCaDtkcw9aL+c5y8jX8MRTBDr+QagGvqYoSfeaT1p+nmn30VhRsfPj3pH6qyZTltgatLdvlsv4QA=="
+                    "submission.publicKeys.AnotherKID",
+                    anotherPublicEcKey
                 )
             } catch (e: NoSuchAlgorithmException) {
                 throw RuntimeException(e)
             }
         }
 
-        private fun generateJwtClaims(date: Date, jti: String): JWTClaimsSet {
+        private fun generateJwtClaims(date: Date, jti: String): JWTClaimsSet.Builder {
             return JWTClaimsSet.Builder()
                 .jwtID(jti)
                 .issueTime(date)
                 .issuer("SIDEP")
-                .build()
         }
 
-        private fun generateJwtHeader(kid: String): JWSHeader {
+        private fun generateJwtHeader(kid: String): JWSHeader.Builder {
             return JWSHeader.Builder(JWSAlgorithm.ES256)
                 .keyID(kid)
                 .type(JOSEObjectType.JWT)
-                .build()
         }
 
-        fun givenJwtWithIncorrectFields(iat: Any? = Date.from(Instant.now()), jti: Any? = UUID.randomUUID()): String {
-            val jwtClaim = JWTClaimsSet.Builder()
-                .claim("iat", iat)
-                .claim("jti", jti)
-                .build()
-            val jwtHeader = generateJwtHeader("TousAntiCovidKID")
-            val jwt = SignedJWT(jwtHeader, jwtClaim)
-            jwt.sign(ECDSASigner(defaultEcKey.toECPrivateKey()))
+        fun givenJwtBuilder(
+            headerBuilder: JWSHeader.Builder = generateJwtHeader("TousAntiCovidKID"),
+            claimsBuilder: JWTClaimsSet.Builder = generateJwtClaims(Date.from(Instant.now()), "TousAntiCovidJti"),
+        ): String {
+            val jwt = SignedJWT(headerBuilder.build(), claimsBuilder.build())
+            jwt.sign(ECDSASigner(tacEcKey.toECPrivateKey()))
 
             return jwt.serialize()
         }
 
-        fun givenJwtWithMissingIat(): String {
-            val jwtClaim = JWTClaimsSet.Builder()
-                .jwtID("TousAntiCovidJti")
-                .issuer("SIDEP")
-                .build()
-            val jwtHeader = generateJwtHeader("TousAntiCovidKID")
-            val jwt = SignedJWT(jwtHeader, jwtClaim)
-            jwt.sign(ECDSASigner(defaultEcKey.toECPrivateKey()))
-
-            return jwt.serialize()
-        }
-
-        fun givenJwtWithMissingJti(): String {
-            val jwtClaim = JWTClaimsSet.Builder()
-                .issueTime(Date.from(Instant.now()))
-                .issuer("SIDEP")
-                .build()
-            val jwtHeader = generateJwtHeader("TousAntiCovidKID")
-            val jwt = SignedJWT(jwtHeader, jwtClaim)
-            jwt.sign(ECDSASigner(defaultEcKey.toECPrivateKey()))
-
-            return jwt.serialize()
-        }
-
-        fun givenJwtWithMissingKid(): String {
-            val jwtClaim = generateJwtClaims(Date.from(Instant.now()), "TousAntiCovidJti")
-            val jwtHeader = JWSHeader.Builder(JWSAlgorithm.ES256)
-                .type(JOSEObjectType.JWT)
-                .build()
-            val jwt = SignedJWT(jwtHeader, jwtClaim)
-            jwt.sign(ECDSASigner(defaultEcKey.toECPrivateKey()))
-
-            return jwt.serialize()
-        }
-
-        fun givenValidJwt(
+        fun givenJwt(
             issuedAt: Instant = Instant.now(),
             jti: String = "TousAntiCovidJti",
             kid: String = "TousAntiCovidKID",
-            privateKey: ECPrivateKey = defaultEcKey.toECPrivateKey()
+            privateKey: ECPrivateKey = tacEcKey.toECPrivateKey()
         ): String {
 
             val jwtClaim = generateJwtClaims(Date.from(issuedAt), jti)
             val jwtHeader = generateJwtHeader(kid)
 
-            val jwt = SignedJWT(jwtHeader, jwtClaim)
+            val jwt = SignedJWT(jwtHeader.build(), jwtClaim.build())
             jwt.sign(ECDSASigner(privateKey))
 
             return jwt.serialize()
