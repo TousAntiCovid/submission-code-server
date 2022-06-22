600687d328dd68df06d34bbf51db90fe4e6a7eb2
Jujube Orange <13631-x-JOrang@users.noreply.gitlab.inria.fr>
2022-06-22 09:45:38 +0000

    test(metrics): lightweight test implementation
    Take a snapshot of metric counters before test execution and expose a utility function to verify how many the counter incremented.

--- src/test/kotlin/fr/gouv/stopc/submissioncode/service/MetricsServiceTest.kt
+++ src/test/kotlin/fr/gouv/stopc/submissioncode/service/MetricsServiceTest.kt
@@ -2,10 +2,9 @@ package fr.gouv.stopc.submissioncode.service
 
 import fr.gouv.stopc.submissioncode.test.IntegrationTest
 import fr.gouv.stopc.submissioncode.test.JWTManager.Companion.givenJwt
-import fr.gouv.stopc.submissioncode.test.MetricsManager.Companion.getCodeVerificationCount
+import fr.gouv.stopc.submissioncode.test.MetricsManager.Companion.assertThatMetricCounterIncrement
 import fr.gouv.stopc.submissioncode.test.PostgresqlManager.Companion.givenTableSubmissionCodeContainsCode
 import fr.gouv.stopc.submissioncode.test.When
-import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
@@ -34,21 +33,16 @@ class MetricsServiceTest {
     )
     fun can_increment_codes_counters(validCode: String, invalidCode: String, codeType: String) {
 
-        val invalidCodeVerificationCountBefore = getCodeVerificationCount("submission.verify.code", "valid", "true", "code type", codeType)
-        val validCodeVerificationCountBefore = getCodeVerificationCount("submission.verify.code", "valid", "false", "code type", codeType)
-
         When()
             .get("/api/v1/verify?code={code}", validCode)
 
         When()
             .get("/api/v1/verify?code={code}", invalidCode)
 
-        assertThat(getCodeVerificationCount("submission.verify.code", "valid", "true", "code type", codeType))
-            .describedAs("Valid code verification count for type $codeType should have been incremented (was $validCodeVerificationCountBefore) ")
-            .isEqualTo(validCodeVerificationCountBefore + 1)
-        assertThat(getCodeVerificationCount("submission.verify.code", "valid", "false", "code type", codeType))
-            .describedAs("Invalid code verification count for type $codeType should have been incremented (was $invalidCodeVerificationCountBefore) ")
-            .isEqualTo(invalidCodeVerificationCountBefore + 1)
+        assertThatMetricCounterIncrement("submission.verify.code", "valid", "true", "code type", codeType)
+            .isEqualTo(1.0)
+        assertThatMetricCounterIncrement("submission.verify.code", "valid", "false", "code type", codeType)
+            .isEqualTo(1.0)
     }
 
     @Test
@@ -57,9 +51,6 @@ class MetricsServiceTest {
         val validJwt = givenJwt()
         val invalidJwt1 = givenJwt(jti = null)
         val invalidJwt2 = givenJwt(kid = null)
-
-        val validJwtVerificationCountBefore = getCodeVerificationCount("submission.verify.code", "valid", "true", "code type", "JWT")
-        val invalidJwtVerificationCountBefore = getCodeVerificationCount("submission.verify.code", "valid", "false", "code type", "JWT")
 
         When()
             .get("/api/v1/verify?code={jwt}", validJwt)
@@ -70,11 +61,9 @@ class MetricsServiceTest {
         When()
             .get("/api/v1/verify?code={jwt}", invalidJwt2)
 
-        assertThat(getCodeVerificationCount("submission.verify.code", "valid", "true", "code type", "JWT"))
-            .describedAs("Valid code verification count for type JWT should have been incremented (was $validJwtVerificationCountBefore) ")
-            .isEqualTo(validJwtVerificationCountBefore + 1)
-        assertThat(getCodeVerificationCount("submission.verify.code", "valid", "false", "code type", "JWT"))
-            .describedAs("Invalid code verification count for type JWT should have been incremented (was $invalidJwtVerificationCountBefore) ")
-            .isEqualTo(invalidJwtVerificationCountBefore + 2)
+        assertThatMetricCounterIncrement("submission.verify.code", "valid", "true", "code type", "JWT")
+            .isEqualTo(1.0)
+        assertThatMetricCounterIncrement("submission.verify.code", "valid", "false", "code type", "JWT")
+            .isEqualTo(2.0)
     }
 }
--- src/test/kotlin/fr/gouv/stopc/submissioncode/test/MetricsManager.kt
+++ src/test/kotlin/fr/gouv/stopc/submissioncode/test/MetricsManager.kt
@@ -1,6 +1,10 @@
 package fr.gouv.stopc.submissioncode.test
 
+import io.micrometer.core.instrument.Counter
+import io.micrometer.core.instrument.Meter
 import io.micrometer.core.instrument.MeterRegistry
+import org.assertj.core.api.AbstractDoubleAssert
+import org.assertj.core.api.Assertions.assertThat
 import org.springframework.beans.factory.getBean
 import org.springframework.test.context.TestContext
 import org.springframework.test.context.TestExecutionListener
@@ -11,18 +15,20 @@ class MetricsManager : TestExecutionListener {
 
         private lateinit var meterRegistry: MeterRegistry
 
-        fun getCodeVerificationCount(name: String, vararg tags: String): Int = try {
-            meterRegistry
-                .get(name)
-                .tags(*tags)
-                .counter()
-                .count().toInt()
-        } catch (e: NullPointerException) {
-            0
+        private lateinit var countersSnapshot: Map<Meter.Id, Double>
+
+        fun assertThatMetricCounterIncrement(name: String, vararg tags: String): AbstractDoubleAssert<*> {
+            val counter = meterRegistry.counter(name, *tags)
+            val previousValue = countersSnapshot[counter.id] ?: 0.0
+            return assertThat(counter.count() - previousValue)
         }
     }
 
     override fun beforeTestMethod(testContext: TestContext) {
         meterRegistry = testContext.applicationContext.getBean(MeterRegistry::class)
+
+        countersSnapshot = meterRegistry.meters
+            .filterIsInstance<Counter>()
+            .associate { it.id to it.count() }
     }
 }
